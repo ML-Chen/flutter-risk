57558a23878e5de13ba89f9585f4ea853c014bd1
Michael <flamingdescent@gmail.com>
2019-03-18 16:51:55 -0400

    Basic UI for home page and lobby

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: SafeArea(
         child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
           children: <Widget>[
             SizedBox(height: 80.0),
             Column(
@@ -29,6 +29,7 @@ class _HomePageState extends State<HomePage> {
             ),
             SizedBox(height: 120.0),
             TextField(
+              // TODO: store the name somewhere
               decoration: InputDecoration(
                 labelText: 'Enter Name'
               )
@@ -39,7 +40,7 @@ class _HomePageState extends State<HomePage> {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => LoginPage())
+                  MaterialPageRoute(builder: (context) => LobbyPage())
                 );
               }
             )
@@ -59,23 +60,89 @@ class _LobbyPageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      endDrawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            // TODO: populate drawer
+            DrawerHeader(
+              child: Text('People')
+            ),
+            ListTile(
+              title: Text('What up')
+            )
+          ]
+        )
+      ),
       appBar: AppBar(
         title: Text('Rooms'),
         actions: <Widget>[
+          Builder(
+            builder: (context) => IconButton(
+              icon: Icon(Icons.people),
+              onPressed: () => Scaffold.of(context).openEndDrawer(),
+              tooltip: 'Players'
+            )
+          ),
           IconButton(
             icon: Icon(Icons.add),
-            tooltip: 'Add room',
-            onPressed: () {
-              
-            }
+            tooltip: 'Create Room',
+            onPressed: _createRoomDialog(context)
           )
         ]
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, position) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              // TODO: fill this with non-filler
+              child: Text(position.toString(), style: TextStyle(fontSize: 22.0))
+            )
+          );
+        }
       )
+      // TODO: snackbar about connecting to server or whatever
     );
-    // Title bar: Rooms with add button
-    // List
-      // if no rooms: No rooms yet. Create one!
-      // Lonely users with no room
-    // Dialog: Create room
   }
 }
+
+_createRoomDialog(BuildContext context) {
+  return showDialog<String>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        contentPadding: const EdgeInsets.all(16.0),
+        content: Row(
+          children: <Widget>[
+            Expanded(
+              child: TextField(
+                autofocus: true
+              )
+            )
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+              child: const Text('CANCEL'),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+          FlatButton(
+              child: const Text('CREATE'),
+              onPressed: () {
+                Navigator.pop(context);
+                // TODO: handle creating room
+              })
+        ]
+      );
+    }
+  );
+}
+
+/* References
+https://github.com/flutter/flutter/issues/19606
+https://stackoverflow.com/questions/51957960/how-to-change-the-enddrawer-icon-in-flutter
+https://flutter.dev/docs/cookbook/design/drawer
+https://medium.com/@DakshHub/flutter-displaying-dynamic-contents-using-listview-builder-f2cedb1a19fb (haven't read yet)
+*/
