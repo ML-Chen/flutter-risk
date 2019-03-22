988ed7e505e8da7d32e9c293f7d399ecd29dbbeb
Michael Chen <mchen419>
2019-03-22 13:07:22 -0400

    Copied Messages.scala, partially converted

--- lib/packets.dart
+++ lib/packets.dart
@@ -1,293 +1,146 @@
-// import 'dart:convert';
+abstract class AuthenticatedMsg { String token; }
 
-// final jsonEncoder = JsonEncoder();
+// Messages for which actor
+abstract class RootMsg
+abstract class AuthenticatedRootMsg extends RootMsg with AuthenticatedMsg
 
-class Token {
-	String token, publictoken;
-  Token(this.token, this.publictoken);
+abstract class RoomMsg extends AuthenticatedMsg { String roomId }
+// Client tries to join room
+class JoinRoom extends RoomMsg with SerializableInEvent {
+	String roomId, String token;
+	JoinRoom(this.roomId, this.token);
+}
+// Client marks themself ready
+class ClientReady extends RoomMsg with SerializableInEvent {
+	String roomId, String token;
+	ClientReady(this.roomId, this.token);
+}
+class StartGame extends RoomMsg with SerializableInEvent {
+	String roomId, String token;
+	StartGame(this.roomId, this.token);
+}
+class LeaveRoom extends RoomMsg with SerializableInEvent {
+	String roomId, String token;
+	LeaveRoom(this.roomId, this.token);
 }
 
-class Ping {
-	String msg;
-	Ping() {
-		this.msg = "Ping";
-	}
+class ForwardToGame(String token, String gameId, GameMsg msg) extends AuthenticatedMsg with SerializableInEvent
+class ForwardToChat(String token, ChatMsg msg) extends AuthenticatedMsg with SerializableInEvent
+
+class UserMessage(String senderName, String publicToken, String message, String timeStamp) extends OutEvent
+class RoomMessage(String senderName, String message, String timeStamp) extends OutEvent
+
+class NotifyGameStarted(GameState state) extends OutEvent
+class SendMapResource(MapResource resource) extends OutEvent
+class NotifyGameState(GameState state) extends OutEvent
+class NotifyGameStart(GameState state) extends OutEvent
+class NotifyTurn(String publicToken) extends OutEvent
+
+// Messages that are sent to the client
+abstract class OutEvent
+
+class NotifyClientsChanged(List<ClientBrief> strings) extends OutEvent
+
+class NotifyRoomsChanged(List<RoomBrief> rooms) extends OutEvent
+
+class NotifyRoomStatus(RoomStatus roomStatus) extends OutEvent
+
+class Token(String token, String publicToken) extends OutEvent
+
+class CreatedRoom(String token) extends OutEvent
+
+class JoinedRoom(String token, String playerToken) extends OutEvent
+
+class NameCheckResult(Boolean available, String name) extends OutEvent
+
+class NameAssignResult(Boolean success, String name, String message = "") extends OutEvent
+
+class RoomCreationResult(Boolean success, String message = "") extends OutEvent
+
+class Ok(String msg) extends OutEvent
+
+class Err(String msg) extends OutEvent
+
+class Ping(String msg) extends OutEvent
+
+class Kill(String msg) extends OutEvent
+
+// Messages which are read (including sent from ourself to ourself
+abstract class InEvent
+
+// Messages which are sent from the client, and can be deserialized
+abstract class SerializableInEvent extends InEvent
+
+// Client first connected, store ActorRef
+class RegisterClient(Client client, ActorRef actor) extends InEvent with RootMsg
+
+// Client sends token to "relogin", empty for new client
+class SetToken(String oldToken, String newToken) extends InEvent with RootMsg
+
+// KeepAlive to kill dead clients
+class KeepAliveTick() extends InEvent with RootMsg
+
+// Client request to list rooms
+class ListRoom(String token) extends AuthenticatedRootMsg with SerializableInEvent
+
+// Client request to validate a name's availability
+class CheckName(String token, String name) extends AuthenticatedRootMsg with SerializableInEvent
+
+// Client response to our ping
+class Pong(String token) extends AuthenticatedRootMsg with SerializableInEvent
+
+// Client tries to assign name
+class AssignName(String name, String token) extends AuthenticatedRootMsg with SerializableInEvent
+
+// Client tries to create room
+class CreateRoom(String roomName, String token) extends AuthenticatedRootMsg with SerializableInEvent
+
+class SerializableInEvent {
+  static final SerializableInEvent _singleton = new MyClass._internal();
+
+  factory SerializableInEvent() {
+    return _singleton;
+  }
+
+	var assignNameRead = Json.reads[AssignName];
+  var joinRoomRead = Json.reads[JoinRoom];
+  var createRoomRead = Json.reads[CreateRoom];
+  var readyRead = Json.reads[ClientReady];
+  var startGameRead = Json.reads[StartGame];
+  var pongRead = Json.reads[Pong];
+  var listRoomRead = Json.reads[ListRoom];
+  var checkNameRead = Json.reads[CheckName];
+  var leaveRoomRead = Json.reads[LeaveRoom];
+  var gameMsgRead = SerializableGameMsg.gameMsgRead;
+  var chatMsgRead = SerializableChatMsg.chatMsgRead;
+  var forwardToGameRead = Json.reads[ForwardToGame];
+  var forwardToChatRead = Json.reads[ForwardToChat];
+  var serializableInEventRead = Json.reads[SerializableInEvent];
 }
 
-class Pong {
-	String msg;
-	Pong() {
-		this.msg = "Pong";
-	}
+object OutEvent {
+  var notifyClientsChangedWrite = Json.writes[NotifyClientsChanged]
+  var notifyRoomsChangedWrite = Json.writes[NotifyRoomsChanged]
+  var notifyRoomStatusWrite = Json.writes[NotifyRoomStatus]
+  var tokenWrite = Json.writes[Token]
+  var okWrite = Json.writes[Ok]
+  var createdRoomWrite = Json.writes[CreatedRoom]
+  var joinedRoomWrite = Json.writes[JoinedRoom]
+  var pingWrite = Json.writes[Ping]
+  var errWrite = Json.writes[Err]
+  var killWrite = Json.writes[Kill]
+  var userMessageWrite = Json.writes[UserMessage]
+  var roomMessageWrite = Json.writes[RoomMessage]
+  var nameCheckResultWrite = Json.writes[NameCheckResult]
+  var nameAssignResultWrite = Json.writes[NameAssignResult]
+  var roomCreationResult = Json.writes[RoomCreationResult]
+  var sendMapResourceWrite = Json.writes[SendMapResource]
+
+  var notifyGameStateWrite = Json.writes[NotifyGameState]
+  var notifyGameStartedWrite = Json.writes[NotifyGameStarted]
+  var notifyGameStartWrite = Json.writes[NotifyGameStart]
+  var notifyTurnWrite = Json.writes[NotifyTurn]
+
+  var outEventFormat = Json.writes[OutEvent]
+  var messageFlowTransformer = MessageFlowTransformer.jsonMessageFlowTransformer[SerializableInEvent, OutEvent]
 }
-
-class CreateRoom {
-	String roomName, token;
-	CreateRoom(this.roomName, this.token);
-}
-
-/*
-class ListRoom {
-	var token, _type;
-
-	ListRoom(token) {
-		this.token = token;
-		this._type = 'ListRoom';
-	}
-}
-
-class NotifyRoomsChanged {
-	var rooms;
-
-	NotifyRoomsChanged(this.rooms);
-	NotifyRoomsChanged.fromOther(other) {
-		this.rooms = other.rooms;
-	}
-}
-
-class NotifyClientsChanged {
-	var players;
-
-	NotifyClientsChanged(this.players);
-}
-
-class RoomStatusUpdate {
-	var roomName, roomId, clientStatus;
-
-	RoomStatusUpdate(this.roomName, this.roomId, this.clientStatus);
-	RoomStatusUpdate.fromOther(other) {
-		this.roomName = other.roomStatus.name;
-		this.roomId = other.roomStatus.roomId;
-		this.clientStatus = other.roomStatus.clientStatus;
-	}
-}
-
-class NameAssignResult {
-	var success, name, message;
-
-	NameAssignResult(this.success, this.name, this.message);
-	NameAssignResult.fromOther(other) {
-		this.success = other.success;
-		this.name = other.name;
-		this.message = other.message;
-	}
-}
-
-class NameCheckResult {
-	var available, name;
-
-	NameCheckResult(this.available, this.name);
-	NameCheckResult.fromOther(other) {
-		this.available = other.available;
-		this.name = other.name;
-	}
-}
-
-class RoomCreationResult {
-	var success, message;
-	
-	RoomCreationResult(this.success, this.message);
-	RoomCreationResult.fromOther(other) {
-		this.success = other.success;
-		this.message = other.message;
-	}
-}
-
-class CreatedRoom {
-	var token;
-
-	CreatedRoom(this.token);
-	CreatedRoom.fromOther(other) {
-		this.token = other.token;
-	}
-}
-
-class JoinedRoom {
-	var roomId;
-
-	JoinedRoom(this.roomId);
-	JoinedRoom.fromOther(other) {
-		this.roomId = other.token;
-		this.playerToken = other.playerToken;
-	}
-}
-
-class GameState {
-	var players, map;
-
-	GameState(this.players, this.map);
-	GameState.fromOther(other) {
-		this.players = other.state.players;
-		this.map = other.state.map;
-	}
-}
-
-class MapResource {
-	var viewBox, territories;
-
-	MapResource(this.viewBox, this.territories);
-	MapResource.fromOther(other) {
-		print(jsonEncoder.convert(other));
-		this.viewBox = other.resource.viewBox;
-		this.territories = other.resource.territories;
-	}
-}
-
-class NotifyTurn {
-	var publicToken;
-
-	NotifyTurn(this.publicToken);
-	NotifyTurn.fromOther(other) {
-		print(jsonEncoder.convert(other));
-		this.publicToken = other.publicToken;
-	}
-}
-
-class Message {
-	var _type, token, territoryId;
-
-	Message(this._type, this.token, this.territoryId);
-}
-
-class PlaceArmy {
-	var token, gameId, msg, _type;
-
-	PlaceArmy(token, gameId, territoryId) {
-		this.token = token;
-		this.gameId = gameId;
-		this.msg = Message('actors.PlaceArmy', token, territoryId);
-		this._type = 'actors.ForwardToGame';
-	}
-}
-
-class Err {
-	var message;
-
-	Err(this.message);
-	Err.fromOther(other) {
-		this.message = other.message;
-	}
-}
-
-class ClientReady {
-	var token, roomId, _type;
-
-	ClientReady(token, roomId) {
-		this.token = token;
-		this.roomId = roomId;
-		this._type = 'actors.ClientReady';
-	}
-}
-
-class StartGame {
-	var token, roomId, _type;
-
-	StartGame(token, roomId) {
-		this.token = token;
-		this.roomId = roomId;
-		this._type = 'actors.StartGame';
-	}
-}
-
-class CreateRoom {
-	var token, roomId, _type;
-
-	CreateRoom(token, roomName) {
-		this.token = token;
-		this.roomName = roomName;
-		this._type = 'actors.CreateRoom';
-	}
-}
-
-class JoinRoom {
-	var token, roomId, _type;
-
-	JoinRoom(token, roomId) {
-		this.token = token;
-		this.roomId = roomId;
-		this._type = 'actors.JoinRoom';
-	}
-}
-
-class CheckName {
-	var token, roomId, _type;
-
-	CheckName(token, name) {
-		this.token = token;
-		this.name = name;
-		this._type = 'actors.CheckName';
-	}
-}
-
-class AssignName {
-	var token, roomId, _type;
-
-	AssignName(token, name) {
-		this.token = token;
-		this.name = name;
-		this._type = 'actors.AssignName';
-	}
-}
-
-processMessage(store, socket, message) {
-	switch (message._type) {
-		case 'actors.Token':
-			const packet = new Token(message);
-			store.commit(types.SET_TOKEN, packet);
-			store.dispatch('gameListRoom', socket);
-			break;
-		case 'actors.Ping':
-			socket.sendObj(new Pong(store.state.game.token));
-			break;
-		case 'actors.NotifyRoomsChanged':
-			const roomsChanged = new NotifyRoomsChanged(message);
-			store.commit(types.GAME_ROOMS_CHANGED, roomsChanged);
-			break;
-		case 'actors.NameCheckResult':
-			const nameCheckResult = new NameCheckResult(message);
-			if (store.state.game.displayName.name === nameCheckResult.name) {
-				store.commit(types.VALIDATE_GAME_NAME, nameCheckResult.available ? 'true' : 'false');
-			}
-			break;
-		case 'actors.CreatedRoom':
-			store.commit(types.GAME_ROOM_CREATED, new CreatedRoom(message));
-			break;
-		case 'actors.JoinedRoom':
-			store.commit(types.GAME_ROOM_JOIN, new JoinedRoom(message));
-			break;
-		case 'actors.RoomCreationResult':
-			store.commit(types.GAME_ROOM_CREATION_RESULT_OCCURRED, new RoomCreationResult(message));
-			break;
-		case 'actors.NotifyClientsChanged':
-			store.commit(types.GAME_PLAYER_LIST_CHANGED, new NotifyClientsChanged(message));
-			break;
-		case 'actors.NotifyRoomStatus':
-			store.commit(types.GAME_ROOM_STATUS_CHANGED, new RoomStatusUpdate(message));
-			break;
-		case 'actors.NameAssignResult':
-			const nameAssignmentResult = new NameAssignResult(message);
-			if (store.state.game.displayName.name == nameAssignmentResult.name) {
-				store.commit(types.COMMIT_GAME_NAME, nameAssignmentResult);
-			}
-			break;
-		case 'actors.NotifyGameStarted':
-			store.commit(types.GAME_STARTED, new GameState(message));
-			break;
-		case 'actors.NotifyGameState':
-			store.commit(types.GAME_STATE, new GameState(message));
-			break;
-		case 'actors.SendMapResource':
-			store.commit(types.MAP_RESOURCE, new MapResource(message));
-			break;
-		case 'actors.NotifyTurn':
-			store.commit(types.NOTIFY_TURN, new NotifyTurn(message));
-			break;
-		case 'actors.Err':
-			print('error', new Err(message).message, 'Error from Server');
-			break;
-		default:
-			print('info', jsonEncoder.convert(message), 'Un-parsed Socket Message:');
-			print(jsonEncoder.convert(message));
-	}
-}
-*/
