c1fc813ac20ae088130cb0a972786cd5389cca8e
Michael Chen <mchen419>
2019-03-22 14:50:46 -0400

    Delete actor files

--- lib/actors/actors.dart
+++
@@ -1,7 +0,0 @@
-// https://stackoverflow.com/questions/13876879/how-do-you-namespace-a-dart-class
-library actors;
-
-part "ChatActor.dart";
-part "GameActor.dart";
-part "Messages.dart";
-part "RootActor.dart";
